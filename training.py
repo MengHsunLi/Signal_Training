6b3323be460b42a529e7945ca48bf1f43cde6105
MengHsunLi <u10416039@go.utaipei.edu.tw>
2018-07-31 15:45:33 +0800

    Add second wifi for device

--- training.py
+++ training.py
@@ -4,8 +4,10 @@ ap_SSID = pd.Series(['AP1', 'AP2', 'AP3', 'AP4', 'AP5'])
 ap_BSSID = pd.Series(['00:1b:2f:a8:e5:21', '00:1b:2f:a8:e5:22', '00:1b:2f:a8:e5:23', '00:1b:2f:a8:e5:24', '00:1b:2f:a8:e5:25'])
 ap_isSecure = pd.Series([True, False, True, False, False])
 loc = pd.Series(['Loc1', 'Loc2', 'Loc3', 'Loc4'])
-device_wifi = pd.Series(['AP2', 'AP3', 'AP1', 'AP5'])
-device_wifi_signal = pd.Series(['0.8197', '0.9', '0.8882', '0.8882'])
+device_wifi_first = pd.Series(['AP2', 'AP3', 'AP1', 'AP5'])
+device_wifi_second = pd.Series(['AP3', 'AP5', 'AP4', 'AP4'])
+device_wifi_signal_first = pd.Series(['0.8197', '0.9', '0.8882', '0.8882'])
+device_wifi_signal_second = pd.Series(['0.7764', '0.8197', '0.8586', '0.8586'])
 device_loc = pd.Series(['Loc1', 'Loc2', 'Loc3', 'Loc4'])
 location_x = pd.Series(['0', '1', '0', '1'])
 location_y = pd.Series(['0', '0', '1', '1'])
@@ -23,19 +25,21 @@ location = pd.DataFrame({'Location':loc, 'Location_X':location_x, 'Location_Y':l
 #   Loc2       1          0
 #   Loc3       0          1
 #   Loc4       1          1
-device = pd.DataFrame({'WIFI_SSID':device_wifi,'WIFI_Signal':device_wifi_signal , 'Location':device_loc})
-# Location WIFI_SSID WIFI_Signal
-#   Loc1      AP2      0.8197
-#   Loc2      AP3      0.9000
-#   Loc3      AP1      0.8882
-#   Loc4      AP5      0.8882
+device = pd.DataFrame({'WIFI_SSID_First':device_wifi_first,'WIFI_Signal_First':device_wifi_signal_first,
+                        'WIFI_SSID_Second':device_wifi_second, 'WIFI_Signal_Second':device_wifi_signal_second,
+                        'Location':device_loc})
+# Location WIFI_SSID_First WIFI_SSID_Second WIFI_Signal_First WIFI_Signal_Second
+#   Loc1         AP2             AP3             0.8197             0.7764
+#   Loc2         AP3             AP5             0.9000             0.8197
+#   Loc3         AP1             AP4             0.8882             0.8586
+#   Loc4         AP5             AP4             0.8882             0.8586
 
-device_wifi_bssid = []
-device_wifi_isSecure = []
-for item in device['WIFI_SSID']:
+device_wifi_bssid_first = []
+device_wifi_isSecure_first = []
+for item in device['WIFI_SSID_First']:
     wifi_mask = ap['SSID'].isin([item])
-    device_wifi_bssid.append(ap[wifi_mask].iloc[0]['BSSID'])
-    device_wifi_isSecure.append(ap[wifi_mask].iloc[0]['Secure'])
+    device_wifi_bssid_first.append(ap[wifi_mask].iloc[0]['BSSID'])
+    device_wifi_isSecure_first.append(ap[wifi_mask].iloc[0]['Secure'])
 device_loc_x = []
 device_loc_y = []
 for item in device['Location']:
@@ -43,17 +47,25 @@ for item in device['Location']:
     device_loc_x.append(location[loc_mask].iloc[0]['Location_X'])
     device_loc_y.append(location[loc_mask].iloc[0]['Location_Y'])
 
-device.insert(loc=3, column='WIFI_Secure', value=device_wifi_isSecure)
-device.insert(loc=3, column='WIFI_BSSID', value=device_wifi_bssid)
+device.insert(loc=5, column='WIFI_Secure_First', value=device_wifi_isSecure_first)
+device.insert(loc=5, column='WIFI_BSSID_First', value=device_wifi_bssid_first)
 device.insert(loc=1, column='Location_Y', value=device_loc_y)
 device.insert(loc=1, column='Location_X', value=device_loc_x)
-# Location  Location_X Location_Y WIFI_SSID WIFI_Signal     WIFI_BSSID     WIFI_Secure
-#   Loc1        0          0         AP2      0.8197    00:1b:2f:a8:e5:22     False
-#   Loc2        1          0         AP3      0.9000    00:1b:2f:a8:e5:23     True
-#   Loc3        0          1         AP1      0.8882    00:1b:2f:a8:e5:21     True
-#   Loc4        1          1         AP5      0.8882    00:1b:2f:a8:e5:25     False
+# Location  Location_X Location_Y WIFI_SSID_First WIFI_Signal_First     WIFI_BSSID     WIFI_Secure
+#   Loc1        0          0            AP2            0.8197        00:1b:2f:a8:e5:22     False
+#   Loc2        1          0            AP3            0.9000        00:1b:2f:a8:e5:23     True
+#   Loc3        0          1            AP1            0.8882        00:1b:2f:a8:e5:21     True
+#   Loc4        1          1            AP5            0.8882        00:1b:2f:a8:e5:25     False
 
-device_wifi_one_hot = pd.get_dummies(device['WIFI_SSID'])
+device_wifi_one_hot_first = pd.get_dummies(device['WIFI_SSID_First'])
+for item in ap_SSID:
+  if item not in device_wifi_one_hot_first:
+    device_wifi_one_hot_first[item] = [0, 0, 0, 0]
+device_wifi_one_hot_second = pd.get_dummies(device['WIFI_SSID_Second'])
+for item in ap_SSID:
+  if item not in device_wifi_one_hot_second:
+    device_wifi_one_hot_second[item] = [0, 0, 0, 0]
+device_wifi_one_hot = device_wifi_one_hot_first + device_wifi_one_hot_second
 device = device.join(device_wifi_one_hot)
 
 print("ap:")
