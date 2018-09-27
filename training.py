06dd1428396c3c0e3c6d267cb0f011cd87486e9d
MengHsunLi <u10416039@go.utaipei.edu.tw>
2018-09-27 14:17:19 +0800

    WIFI_Signal_One_Hot weight, training feature

--- training.py
+++ training.py
@@ -105,10 +105,12 @@ device_wifi_one_hot_first = pd.get_dummies(device['WIFI_SSID_First'])
 for item in ap_SSID:
     if item not in device_wifi_one_hot_first:
         device_wifi_one_hot_first[item] = [0, 0, 0, 0]
+device_wifi_one_hot_first=device_wifi_one_hot_first*3
 device_wifi_one_hot_second = pd.get_dummies(device['WIFI_SSID_Second'])
 for item in ap_SSID:
     if item not in device_wifi_one_hot_second:
         device_wifi_one_hot_second[item] = [0, 0, 0, 0]
+device_wifi_one_hot_second=device_wifi_one_hot_second*2
 device_wifi_one_hot_third = pd.get_dummies(device['WIFI_SSID_Third'])
 for item in ap_SSID:
     if item not in device_wifi_one_hot_third:
@@ -155,3 +157,225 @@ print(newdevice)
 
 
 #Training
+from __future__ import print_function
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+newdevice = newdevice.reindex(np.random.permutation(newdevice.index))
+
+def preprocess_features(device):
+  """Prepares input features from Device data set.
+
+  Args:
+    device: A Pandas DataFrame expected to contain data
+      from the Device data set.
+  Returns:
+    A DataFrame that contains the features to be used for the model, including
+    synthetic features.
+  """
+  selected_features = device[
+    ["Location",
+     "WIFI_SSID_First", "WIFI_SSID_Second", "WIFI_SSID_Third",
+     "WIFI_Signal_First", "WIFI_Signal_Second", "WIFI_Signal_Third",
+     "AP1", "AP2", "AP3", "AP4", "AP5",
+     "Loc1", "Loc2", "Loc3", "Loc4"
+    ]]
+  processed_features = selected_features.copy()
+  return processed_features
+
+def preprocess_targets(device):
+  """Prepares target features (i.e., labels) from Device data set.
+
+  Args:
+    device: A Pandas DataFrame expected to contain data
+      from the Device data set.
+  Returns:
+    A DataFrame that contains the target feature.
+  """
+  output_targets = pd.DataFrame()
+  # Scale the target to be in units of thousands of dollars.
+  output_targets["Location_X"] = (
+    device["Location_X"])
+  return output_targets
+
+# Choose the first 1800 (out of 2000) examples for training.
+training_examples = preprocess_features(newdevice.head(1800))
+training_targets = preprocess_targets(newdevice.head(1800))
+# Choose the last 200 (out of 2000) examples for validation.
+validation_examples = preprocess_features(newdevice.tail(200))
+validation_targets = preprocess_targets(newdevice.tail(200))
+
+correlation_dataframe = training_examples.copy()
+correlation_dataframe["target"] = training_targets["Location_X"]
+correlation_dataframe.corr()
+
+def construct_feature_columns(input_features):
+  """Construct the TensorFlow Feature Columns.
+
+  Args:
+    input_features: The names of the numerical input features to use.
+  Returns:
+    A set of feature columns
+  """
+  return set([tf.feature_column.numeric_column(my_feature)
+              for my_feature in input_features])
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    """Trains a linear regression model.
+
+    Args:
+      features: pandas DataFrame of features
+      targets: pandas DataFrame of targets
+      batch_size: Size of batches to be passed to the model
+      shuffle: True or False. Whether to shuffle the data.
+      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
+    Returns:
+      Tuple of (features, labels) for next data batch
+    """
+
+    # Convert pandas data into a dict of np arrays.
+    features = {key:np.array(value) for key,value in dict(features).items()}
+
+    # Construct a dataset, and configure batching/repeating.
+    ds = Dataset.from_tensor_slices((features,targets)) # warning: 2GB limit
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # Shuffle the data, if specified.
+    if shuffle:
+      ds = ds.shuffle(10000)
+
+    # Return the next batch of data.
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+def train_model(
+    learning_rate,
+    steps,
+    batch_size,
+    training_examples,
+    training_targets,
+    validation_examples,
+    validation_targets):
+  """Trains a linear regression model.
+
+  In addition to training, this function also prints training progress information,
+  as well as a plot of the training and validation loss over time.
+
+  Args:
+    learning_rate: A `float`, the learning rate.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    training_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for training.
+    training_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for training.
+    validation_examples: A `DataFrame` containing one or more columns from
+      `california_housing_dataframe` to use as input features for validation.
+    validation_targets: A `DataFrame` containing exactly one column from
+      `california_housing_dataframe` to use as target for validation.
+
+  Returns:
+    A `LinearRegressor` object trained on the training data.
+  """
+
+  periods = 50
+  steps_per_period = steps / periods
+
+  # Create a linear regressor object.
+  my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  linear_regressor = tf.estimator.LinearRegressor(
+      feature_columns=construct_feature_columns(training_examples),
+      optimizer=my_optimizer
+  )
+
+  # Create input functions.
+  training_input_fn = lambda: my_input_fn(training_examples,
+                                          training_targets["Location_X"],
+                                          batch_size=batch_size)
+  predict_training_input_fn = lambda: my_input_fn(training_examples,
+                                                  training_targets["Location_X"],
+                                                  num_epochs=1,
+                                                  shuffle=False)
+  predict_validation_input_fn = lambda: my_input_fn(validation_examples,
+                                                    validation_targets["Location_X"],
+                                                    num_epochs=1,
+                                                    shuffle=False)
+
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("RMSE (on training data):")
+  training_rmse = []
+  validation_rmse = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    linear_regressor.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period,
+    )
+    # Take a break and compute predictions.
+    training_predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
+    training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+
+    validation_predictions = linear_regressor.predict(input_fn=predict_validation_input_fn)
+    validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+
+    # Compute training and validation loss.
+    training_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(training_predictions, training_targets))
+    validation_root_mean_squared_error = math.sqrt(
+        metrics.mean_squared_error(validation_predictions, validation_targets))
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+    # Add the loss metrics from this period to our list.
+    training_rmse.append(training_root_mean_squared_error)
+    validation_rmse.append(validation_root_mean_squared_error)
+  print("Model training finished.")
+
+
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("RMSE")
+  plt.xlabel("Periods")
+  plt.title("Root Mean Squared Error vs. Periods")
+  plt.tight_layout()
+  plt.plot(training_rmse, label="training")
+  plt.plot(validation_rmse, label="validation")
+  plt.legend()
+
+  return linear_regressor
+
+minimal_features = [
+  "WIFI_Signal_First",
+  "WIFI_Signal_Second",
+  "WIFI_Signal_Third",
+  "AP1", "AP2", "AP3", "AP4", "AP5",
+  "Loc1", "Loc2", "Loc3", "Loc4"
+]
+
+minimal_training_examples = training_examples[minimal_features]
+minimal_validation_examples = validation_examples[minimal_features]
+
+_ = train_model(
+    learning_rate=0.0005,
+    steps=500,
+    batch_size=10,
+    training_examples=minimal_training_examples,
+    training_targets=training_targets,
+    validation_examples=minimal_validation_examples,
+    validation_targets=validation_targets)
