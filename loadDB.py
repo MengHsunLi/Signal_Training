23a9ab52fff222345cf0c0d9b6395084ab6bd162
MengHsunLi <u10416039@go.utaipei.edu.tw>
2018-11-07 20:31:59 +0800

    Modify the feature of listing child contents from database(specific id to all id)

--- loadDB.py
+++ loadDB.py
@@ -2,15 +2,41 @@ from firebase import firebase
 import pandas as pd
 
 url = "https://test-4fef8.firebaseio.com/"
-#url = "https://app01-7a86c.firebaseio.com/"
 db = firebase.FirebaseApplication(url, None)
 Signals = db.get("/Signals", None)
-SSIDs = db.get("/Signals/20181031134650", 'SSIDs')
-BSSIDs = db.get("/Signals/20181031134650", 'BSSIDs')
-Strengths = db.get("/Signals/20181031134650", 'Strengths')
-def printInfo(db):
+def printInfo(infoDB):
+    Keys = pd.Series()
+    i = 0
+    for key,value in infoDB.items():
+        Keys.at[i] = key
+        i += 1
+    for j in range(i):
+        print("===============Info===============")
+        dbStr='%s%s'%('/Signals/', Keys[j])
+        infoDBDevice = db.get('%s%s'%(dbStr,'/Device'), None)
+        infoDBLoc = db.get('%s%s'%(dbStr,'/Location'), None)
+        infoDBLocX = db.get('%s%s'%(dbStr,'/LocationX'), None)
+        infoDBLocY = db.get('%s%s'%(dbStr,'/LocationX'), None)
+        infoDBTime = db.get('%s%s'%(dbStr,'/Time'), None)
+        SSIDs = db.get(dbStr, 'SSIDs')
+        BSSIDs = db.get(dbStr, 'BSSIDs')
+        Strengths = db.get(dbStr, 'Strengths')
+        print("Id:\t", Keys[j])
+        print("Device:\t", infoDBDevice)
+        print("Location:\t", infoDBLoc)
+        print("LocationX:\t", infoDBLocX)
+        print("LocationX:\t", infoDBLocY)
+        print("Time:\t", infoDBTime)
+        print("===============APs================")
+        for k in range(5):
+            printSSID(SSIDs, k)
+            printBSSID(BSSIDs, k)
+            printStrength(Strengths, k)
+            print("----------------------------------")
+        print("==================================")
+def printInfoOrg(infoDB):
     print("===============Info===============")
-    for key,value in db.items():
+    for key,value in infoDB.items():
         print("Id: {}\nDevice: {}\nLocation: {}\nLocationX={}\tLocationY={}\nStamp: {}"
             .format(key,value["Device"],value["Location"],value["LocationX"],value["LocationY"],
                 value["Time"]))
@@ -25,10 +51,3 @@ def printStrength(St, num):
     print("{}: {}".format(d[num],St[d[num]]))
 
 printInfo(Signals)
-print("==================================")
-for i in range(5):
-    printSSID(SSIDs, i)
-    printBSSID(BSSIDs, i)
-    printStrength(Strengths, i)
-    print("----------------------------------")
-print("==================================")
